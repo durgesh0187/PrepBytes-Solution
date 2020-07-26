d1ce1fb8f225404cb4b01d9667d6b762bc59dd9f
Durgesh Kumar <68027282+durgesh0187@users.noreply.github.com>
2020-07-26 22:26:36 +0530

    PrepBytes

---
+++ PrepBuddy_and_Socks.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    if(n==n)
+    {
+      cout<<n+1<<endl;
+    }
+    
+  
+  }
+  return 0;
+}
